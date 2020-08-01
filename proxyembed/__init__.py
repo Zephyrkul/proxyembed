e26ffc9a98dfd4522614947c4a50986e18276a32
zephyrkul <zephyrkul@users.noreply.github.com>
2020-07-31 19:55:04 -0600

    update format, use allowed_mentions

--- proxyembed/__init__.py
+++ proxyembed/__init__.py
@@ -2,27 +2,26 @@
 which respect Red's ctx.embed_requested()"""
 
 import logging
+import re
 from collections import defaultdict
+from typing import NoReturn
 
 import discord
+from babel.dates import format_datetime
 from redbot.core.commands import Context
+from redbot.core.i18n import get_babel_locale
 from redbot.core.utils import chat_formatting as CF
 
 __all__ = ["ProxyEmbed"]
 __author__ = "Zephyrkul"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 LOG = logging.getLogger("red.fluffy.proxyembed")
+LINK_MD = re.compile(r"\[([^\]]+)\]\(([^\)]+)\)")
 
 
-# from: https://stackoverflow.com/a/34445090
-def findall(p, s):
-    """Yields all the positions of
-    the pattern p in the string s."""
-    i = s.find(p)
-    while i != -1:
-        yield i
-        i = s.find(p, i + 1)
+def _links(string: str) -> str:
+    return LINK_MD.sub(r"\1 (<\2>)", string)
 
 
 class _OverwritesEmbed(discord.Embed):
@@ -36,18 +35,24 @@ class _OverwritesEmbed(discord.Embed):
     @property
     def fields(self):
         return defaultdict(
-            lambda: discord.embeds.EmbedProxy(),
+            discord.embeds.EmbedProxy,
             {i: discord.embeds.EmbedProxy(v) for i, v in self._fields.items()},
         )
 
-    def add_field(self, *args, **kwargs):
+    def add_field(self, *args, **kwargs) -> NoReturn:
         raise NotImplementedError("This operation is unsupported for overwrites.")
 
 
 class ProxyEmbed(discord.Embed):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.overwrites = _OverwritesEmbed()
+    def __new__(cls, *args, **kwargs):
+        # d.py likes to call __new__ without __init__
+        self = super().__new__(cls)
+        self._overwrites = _OverwritesEmbed()
+        return self
+
+    @property
+    def overwrites(self):
+        return self._overwrites
 
     @classmethod
     def _get(cls, obj, attr):
@@ -75,72 +80,74 @@ class ProxyEmbed(discord.Embed):
             obj = self._get(obj, attr)
         if overwrite is not self.Empty:
             LOG.debug("Returning overwritten value %r", overwrite)
-            return str(overwrite).strip()
+            return overwrite
         if obj is not self.Empty:
-            return str(obj).strip()
+            return obj
         return self.Empty
 
-    async def send_to(self, ctx: Context, content=None):
+    async def send_to(self, ctx: Context, content: str = None) -> None:
         if await ctx.embed_requested():
             return await ctx.send(content=content, embed=self)
         content = str(content) if content is not None else None
         if content:
-            content = [content, ""]
+            unwrapped = [content, ""]
         else:
-            content = []
-        next_break = False
+            unwrapped = []
         title = self._("title")
         if title:
-            content.append(CF.bold(title))
-            next_break = True
+            unwrapped.append(CF.bold(title))
+        url = self._("url")
+        if url:
+            unwrapped.append(f"> <{url}>")
         name = self._("author.name")
         if name:
-            content.append(CF.italics(name))
-            next_break = True
+            unwrapped.append(CF.italics(name))
+        url = self._("author.url")
+        if url:
+            unwrapped.append(f"> <{url}>")
+        if unwrapped and unwrapped[-1]:
+            unwrapped.append("")
         url = self._("thumbnail.url")
         if url and not url.startswith("attachment://"):
-            content.append(f"<{url}>")
-            next_break = True
+            unwrapped.append(f"<{url}>")
         description = self._("description")
         if description:
-            content.append(CF.box(CF.escape(description, formatting=True)))
-            next_break = False
-        if next_break:
-            content.append("")
-            next_break = False
-        for i in range(len(self._fields)):
+            unwrapped.extend(f"> {line}" for line in description.split("\n"))
+        if unwrapped and unwrapped[-1]:
+            unwrapped.append("")
+        for i in range(len(getattr(self, "_fields", []))):
             inline, name, value = (
                 self._("_fields", i, "inline"),
                 self._("_fields", i, "name"),
                 self._("_fields", i, "value"),
             )
             LOG.debug("index: %r, inline: %r, name: %r, value: %r", i, inline, name, value)
+            name = f"**{name}**"
             if not inline or len(name) + len(value) > 78 or "\n" in name or "\n" in value:
-                content.append(name)
-                blocks = tuple(findall("```", value))
-                if blocks == (0, len(value) - 3):
-                    content.append(value)
-                else:
-                    content.append(CF.box(CF.escape(value, formatting=True)))
-                next_break = False
+                unwrapped.append(name)
+                unwrapped.extend(f"> {line}" for line in value.split("\n"))
             else:
-                content.append(f"{name}: {value}")
-                next_break = True
-        if next_break:
-            content.append("")
-            next_break = False
+                unwrapped.append(f"{name} | {value}")
+        if unwrapped and unwrapped[-1]:
+            unwrapped.append("")
         url = self._("image.url")
         if url and not url.startswith("attachment://"):
-            content.append(f"<{url}>")
-        url = self._("video.url")
-        if url and not url.startswith("attachment://"):
-            content.append(f"<{url}>")
+            unwrapped.append(f"<{url}>")
         text, timestamp = self._("footer.text"), self._("timestamp")
         if text and timestamp:
-            content.append(f"{text} | {timestamp}")
+            ftimestamp = format_datetime(timestamp, locale=get_babel_locale())
+            unwrapped.append(f"{text} • {ftimestamp}")
         elif text:
-            content.append(text)
+            unwrapped.append(text)
         elif timestamp:
-            content.append(f"{timestamp} UTC")
-        content = list(CF.pagify("\n".join(map(str, content)), shorten_by=0))
-        return await ctx.send_interactive(content)
+            ftimestamp = format_datetime(timestamp, locale=get_babel_locale())
+            unwrapped.append(f"{ftimestamp}")
+
+        route = discord.http.Route(
+            "POST", "/channels/{channel_id}/messages", channel_id=ctx.channel.id
+        )
+        pages = CF.pagify(_links("\n".join(map(str, unwrapped))), shorten_by=0)
+        for page in pages:
+            await ctx.bot.http.request(
+                route, json={"content": page, "allowed_mentions": {"parse": []}}
+            )
