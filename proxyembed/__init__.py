786be3f71324ecc44fdbfa95ac726da9c16cd35a
zephyrkul <zephyrkul@users.noreply.github.com>
2020-09-13 20:53:51 -0600

    0.0.3

--- proxyembed/__init__.py
+++ proxyembed/__init__.py
@@ -1,23 +1,27 @@
 """Auto-unwrapping d.py embeds for use with Red,
 which respect Red's ctx.embed_requested()"""
 
+import functools
 import logging
 import re
+import textwrap
 from collections import defaultdict
-from typing import NoReturn, List
+from types import SimpleNamespace
+from typing import Any, Callable, Coroutine, NoReturn, Optional, Union
 
 import discord
 from babel.dates import format_datetime
-from redbot.core.commands import Context
+from redbot.core.bot import Red
 from redbot.core.i18n import get_babel_locale
-from redbot.core.utils import chat_formatting as CF
+from redbot.core.utils.chat_formatting import bold, italics
 
-__all__ = ["ProxyEmbed"]
+__all__ = ["ProxyEmbed", "EmptyOverwrite", "embed_requested"]
 __author__ = "Zephyrkul"
-__version__ = "0.0.3dev1"
+__version__ = "0.0.3"
 
 LOG = logging.getLogger("red.fluffy.proxyembed")
 LINK_MD = re.compile(r"\[([^\]]+)\]\(([^\)]+)\)")
+quote = functools.partial(textwrap.indent, prefix="> ", predicate=lambda l: True)
 
 
 def _links(string: str) -> str:
@@ -43,12 +47,86 @@ class _OverwritesEmbed(discord.Embed):
         raise NotImplementedError("This operation is unsupported for overwrites.")
 
 
+EmptyOverwrite = ""
+
+
+async def embed_requested(
+    __dest: Union[discord.abc.Messageable, discord.Message],
+    /,
+    *,
+    bot: Red = None,
+) -> bool:
+    """
+    Helper method to determine whether to send an embed to any arbitrary destination.
+
+    If neither ``ctx`` nor ``bot`` are passed, only Discord permissions are checked.
+    """
+    # Note: This doesn't handle GroupChannel. Bots can't access GroupChannels.
+    if method := getattr(__dest, "embed_requested", None):
+        return await method()
+    ns: SimpleNamespace
+    if isinstance(__dest, discord.Message):
+        ns = SimpleNamespace(
+            channel=__dest.channel, user=__dest.author, guild=__dest.guild
+        )
+    elif isinstance(__dest, (discord.User, discord.Member)):
+        try:
+            channel = __dest.dm_channel or await __dest.create_dm()
+        except AttributeError:
+            raise TypeError("I can't send DMs to myself.") from None
+        ns = SimpleNamespace(
+            channel=channel,
+            user=__dest,
+            guild=None,
+        )
+    elif isinstance(__dest, discord.DMChannel):
+        ns = SimpleNamespace(channel=__dest, author=__dest.recipient, guild=None)
+    else:
+        if guild := getattr(__dest, "guild", None):
+            # the actual user object here doesn't matter
+            ns = SimpleNamespace(channel=__dest, user=None, guild=guild)
+        else:
+            raise TypeError(f"Unknown destination type {__dest.__class__!r}")
+    if ns.guild and not ns.channel.permissions_for(ns.guild.me).embed_links:
+        return False
+    if not bot:
+        LOG.warning("No bot kwarg provided; only checking permissions")
+        return True
+    return await bot.embed_requested(ns.channel, ns.user)
+
+
 class ProxyEmbed(discord.Embed):
+    """Proxy object for a :class:`discord.Embed` object that unwraps itself into
+    ``content`` automatically if the embed can't / shouldn't be sent.
+
+    Extra Attributes
+    ----------------
+    overwrites: :class:`discord.Embed`
+        A special Embed subclass to indicate what should change if
+        this ProxyEmbed needs to unwrap into text.
+        Ignored completely if this ProxyEmbed sends normally.
+    EmptyOverwrite
+        A sentinel value used by :attr:`overwrites` to send nothing
+        if this ProxyEmbed unwraps into text.
+        Ignored completely if this ProxyEmbed sends normally.
+
+        (Impl. note: This is just the empty string. Use the attribute anyway in case that ever changes.)
+    """
+
+    __slots__ = ("_overwrites", *discord.Embed.__slots__)
+    EmptyOverwrite = EmptyOverwrite
+
     def __new__(cls, *args, **kwargs):
         # d.py likes to call __new__ without __init__
         self = super().__new__(cls)
         self._overwrites = _OverwritesEmbed()
         return self
+
+    @classmethod
+    def from_embed(cls, embed: discord.Embed):
+        if isinstance(embed, cls):
+            return embed
+        return cls.from_dict(embed.to_dict())
 
     @property
     def overwrites(self):
@@ -56,6 +134,8 @@ class ProxyEmbed(discord.Embed):
 
     @classmethod
     def _get(cls, obj, attr):
+        if obj == EmptyOverwrite:
+            return obj
         try:
             obj = getattr(obj, attr)
         except AttributeError:
@@ -79,72 +159,131 @@ class ProxyEmbed(discord.Embed):
                 overwrite = self._get(overwrite, attr)
             obj = self._get(obj, attr)
         if overwrite is not self.Empty:
-            LOG.debug("Returning overwritten value %r", overwrite)
+            LOG.debug(
+                "Returning overwritten value %r for attr ProxyEmbed.%r",
+                overwrite,
+                attrs,
+            )
             return overwrite
         if obj is not self.Empty:
             return obj
         return self.Empty
 
-    async def send_to(self, ctx: Context, content: str = None) -> discord.Message:
-        if await ctx.embed_requested():
-            return await ctx.send(content=content, embed=self)
-        content = str(content) if content is not None else None
-        if content:
-            unwrapped = [content, ""]
+    def to_dict(self):
+        result = super().to_dict()
+        result.pop("overwrites", None)
+        return result
+
+    async def send_to(
+        self,
+        __dest: Union[discord.abc.Messageable, discord.Message],
+        /,
+        content: str = None,
+        *,
+        bot: Red = None,
+        **kwargs,
+    ) -> discord.Message:
+        """
+        Sends this ProxyEmbed to the specified destination.
+
+        If embeds can't / shouldn't be sent, this will unwrap this ProxyEmbed
+        into pure text and send that instead.
+
+        Note that this **does not** pagify large embeds for you.
+        Use pagify on your own if you believe your embed may be too large.
+        """
+        _ = self._
+        if kwargs.pop("embed", self) is not self:
+            raise TypeError("send_to() got an unexpected kwarg 'embed'")
+        bot = bot or getattr(__dest, "bot", None)
+        send: Callable[..., Coroutine[Any, Any, Optional[discord.Message]]]
+        if isinstance(__dest, discord.Message):
+            send = functools.partial(__dest.edit, **kwargs)
         else:
-            unwrapped = []
-        title = self._("title")
+            send = functools.partial(__dest.send, **kwargs)
+        if await embed_requested(__dest, bot=bot):
+            if message := await send(content=content, embed=self):
+                return message
+            else:
+                assert isinstance(__dest, discord.Message)
+                return __dest
+        content = str(content) if content is not None else None
+        unwrapped = []
+        if content:
+            unwrapped.extend([content, ""])
+        title = _("title")
         if title:
-            unwrapped.append(CF.bold(title))
-        url = self._("url")
+            unwrapped.append(bold(title))
+        url = _("url")
         if url:
             unwrapped.append(f"> <{url}>")
-        name = self._("author.name")
+        name = _("author.name")
         if name:
-            unwrapped.append(CF.italics(name))
-        url = self._("author.url")
+            unwrapped.append(italics(name))
+        url = _("author.url")
         if url:
-            unwrapped.append(f"> <{url}>")
+            unwrapped.append(f"<{url}>")
         if unwrapped and unwrapped[-1]:
             unwrapped.append("")
-        url = self._("thumbnail.url")
+        url = _("thumbnail.url")
         if url and not url.startswith("attachment://"):
             unwrapped.append(f"<{url}>")
-        description = self._("description")
+        description = _("description")
         if description:
-            unwrapped.extend(f"> {line}" for line in description.split("\n"))
+            unwrapped.append(quote(description))
         if unwrapped and unwrapped[-1]:
             unwrapped.append("")
         for i in range(len(getattr(self, "_fields", []))):
             inline, name, value = (
-                self._("_fields", i, "inline"),
-                self._("_fields", i, "name"),
-                self._("_fields", i, "value"),
+                _("_fields", i, "inline"),
+                _("_fields", i, "name"),
+                _("_fields", i, "value"),
             )
-            LOG.debug("index: %r, inline: %r, name: %r, value: %r", i, inline, name, value)
+            LOG.debug(
+                "index: %r, inline: %r, name: %r, value: %r", i, inline, name, value
+            )
             name = f"**{name}**"
-            if not inline or len(name) + len(value) > 78 or "\n" in name or "\n" in value:
+            if (
+                not inline
+                or len(name) + len(value) > 78
+                or "\n" in name
+                or "\n" in value
+            ):
                 unwrapped.append(name)
-                unwrapped.extend(f"> {line}" for line in value.split("\n"))
+                unwrapped.append(quote(value))
             else:
                 unwrapped.append(f"{name} | {value}")
         if unwrapped and unwrapped[-1]:
             unwrapped.append("")
-        url = self._("image.url")
+        url = _("image.url")
         if url and not url.startswith("attachment://"):
             unwrapped.append(f"<{url}>")
-        text, timestamp = self._("footer.text"), self._("timestamp")
+        text, timestamp = _("footer.text"), _("timestamp")
         if text and timestamp:
-            ftimestamp = format_datetime(timestamp, format="long", locale=get_babel_locale())
+            ftimestamp = format_datetime(
+                timestamp, format="long", locale=get_babel_locale()
+            )
             unwrapped.append(f"{text} • {ftimestamp}")
         elif text:
             unwrapped.append(text)
         elif timestamp:
-            ftimestamp = format_datetime(timestamp, format="long", locale=get_babel_locale())
+            ftimestamp = format_datetime(
+                timestamp, format="long", locale=get_babel_locale()
+            )
             unwrapped.append(ftimestamp)
 
-        pages = CF.pagify(_links("\n".join(map(str, unwrapped))), shorten_by=0)
-        m = None
-        for page in pages:
-            m = await ctx.send(page, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, users=False))
-        return m
+        if mentions := kwargs.get("allowed_mentions", None):
+            for attr in ("everyone", "roles", "users"):
+                # use == here in case it's default
+                if getattr(mentions, attr) == True:
+                    setattr(mentions, attr, False)
+        else:
+            kwargs["allowed_mentions"] = discord.AllowedMentions(
+                everyone=False, roles=False, users=False
+            )
+
+        if message := await send(_links("\n".join(unwrapped))):
+            return message
+        else:
+            assert isinstance(__dest, discord.Message)
+            return __dest
